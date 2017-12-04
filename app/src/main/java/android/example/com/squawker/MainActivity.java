5c16dc59a3bab5f7e99775f739bbc6c6797179e2
sdtareq <sdtareq@gmail.com>
2017-12-04 23:05:59 +0600

    Provider problem Solved and test for getting extra from FCM

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -86,6 +86,10 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.containsKey("test")) {
+            Log.d(LOG_TAG, "onCreate: Contains - " + extras.getString("test"));
+        }
     }
 
     @Override
